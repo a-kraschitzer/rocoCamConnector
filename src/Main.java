45ea56dda26bd860b612d56f849e33d805d25180
Andreas Kraschitzer <andreas.kraschitzer@gmail.com>
2019-12-14 00:37:07 +0100

    cleaned up Main.java, added possibility for trying out example consumers

--- src/Main.java
+++ src/Main.java
@@ -4,92 +4,69 @@ import net.kraschitzer.roco.consumers.awt.gui.VidFrame;
 import net.kraschitzer.roco.exceptions.FormatException;
 
 import java.io.IOException;
-
 import java.net.SocketException;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
+
+    private enum Mode {
+        FRAME,
+        FILES,
+    }
 
     public static void main(String[] args) throws IOException, FormatException {
-        writeImagesToFile();
+        Scanner s = new Scanner(System.in);
+        String input;
+
+        Mode mode;
+        String ip;
+        String outDir = null;
+
+        System.out.println("Please select the mode you want to start in:");
+        System.out.println("*1 Frame");
+        System.out.println("2 Files");
+        input = s.nextLine();
+        if ("2".equals(input)) {
+            mode = Mode.FILES;
+        } else {
+            mode = Mode.FRAME;
+        }
+
+        if (Mode.FILES.equals(mode)) {
+            System.out.println("Please supply the output directory for the captured files:");
+            outDir = s.nextLine();
+        }
+
+        System.out.println("Please supply the IPv4 address your roco locomotive is connected to:");
+        ip = s.nextLine();
+
+        switch (mode) {
+            case FRAME:
+                showImagesInExtendedFrame(ip);
+                break;
+            case FILES:
+                writeImagesToFile(ip, outDir);
+                break;
+        }
     }
 
-    private static void writeImagesToFile() throws SocketException, FormatException {
-        ImageFileWriter imageFileWriter = new ImageFileWriter("./out");
+    private static void writeImagesToFile(String ip, String outDir) throws SocketException, FormatException {
+        ImageFileWriter imageFileWriter = new ImageFileWriter(outDir);
         ComController comController = new ComController();
-        comController.startLoco(imageFileWriter, "192.168.1.114");
+        comController.startLoco(imageFileWriter, ip);
     }
 
-    private static void showImagesInFrame() throws SocketException, FormatException {
+    private static void showImagesInFrame(String ip) throws SocketException, FormatException {
         VidFrame f = new VidFrame();
         f.setVisible(true);
         ComController comController = new ComController();
-        comController.startLoco(f.getImagePanel(), "192.168.1.114");
+        comController.startLoco(f.getImagePanel(), ip);
     }
 
-    private static void showImagesInExtendedFrame() throws SocketException, FormatException {
+    private static void showImagesInExtendedFrame(String ip) throws SocketException, FormatException {
         VidFrame f = new VidFrame();
         f.setVisible(true);
         ComController comController = new ComController();
-        comController.startLoco(f.getImagePanel(), "192.168.1.114");
+        comController.startLoco(f.getImagePanel(), ip);
     }
-
-    private static void startLocoForImagePanel() {
-//        Scanner s = new Scanner(System.in);
-//        String input = "";
-//
-//        InetAddress address = null;
-//        int port = 0;
-//
-//
-//        System.out.println("Please provide ip range [192.168.0.0]");
-//        //input = s.nextLine();
-//        try {
-//            if (input != null && input.isEmpty()) {
-//                input = "192.168.0.0";
-//            }
-//            address = InetAddress.getByName(input);
-//        } catch (UnknownHostException e) {
-//            System.out.println("invalid input '" + input + "'");
-//        }
-//
-//        System.out.println("Please provide a port for setting up the communication [5152]");
-//        //input = s.nextLine();
-//        try {
-//            if (input != null && input.isEmpty()) {
-//                input = "5152";
-//            }
-//            port = Integer.parseInt(input);
-//        } catch (NumberFormatException e) {
-//            System.out.println("invalid input '" + input + "'");
-//        }
-
-        /*
-        READ FROM INI/cams.ini
-        ArrayList<CamLoco> locos = new ArrayList<>();
-
-        BufferedReader br = FileOperation.loadData("INI/cams.ini");
-        String line = br.readLine();
-
-        while (line != null) {
-            String[] values = line.split(";");
-            locos.add(new CamLoco(values[0], Integer.parseInt(values[1]), Integer.parseInt(values[2]), values[3]));
-            line = br.readLine();
-        }
-
-        ComController com = new ComController();
-
-        for (CamLoco loco : locos) {
-            com.startSpecificLoco(loco);
-
-        }
-         */
-
-
-
-
-//        com.startAllLocos();
-    }
-    
-    
-    
 }
